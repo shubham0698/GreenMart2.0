377531f2092bd6abd3370bf614be573699efa812
shubham0698 <holkarshubham888@gmail.com>
2025-11-14 11:44:50 +0530

    second commit for pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";   // if you're using React
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/<GreenMart2.0>/",
   plugins: [
     react(),        // if applicable
     tailwindcss()   // add this plugin
