2889c4728ceeb345ad5faebfef89686511e0160f
shubham0698 <holkarshubham888@gmail.com>
2025-11-14 12:00:52 +0530

    another attempt

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";   // if you're using React
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/<GreenMart2.0>/",
+  
   plugins: [
     react(),        // if applicable
     tailwindcss()   // add this plugin
   ],
+  base: "/GreenMart2.0/",
 });
